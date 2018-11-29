99fa6474158287017194a5a7f284bf4e1a5f0b77
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2018-11-29 20:44:47 +0800

    add a textcnn model

---
+++ poros/poros_train/some_layer.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+comments
+author: diqiuzhuanzhuan
+email: diqiuzhuanzhuan@gmail.com
+
+"""
+
+import tensorflow as tf
+
+
+def dropout(input_tensor, dropout_prob):
+    """Perform dropout.
+
+    Args:
+      input_tensor: float Tensor.
+      dropout_prob: Python float. The probability of dropping out a value (NOT of
+        *keeping* a dimension as in `tf.nn.dropout`).
+
+    Returns:
+      A version of `input_tensor` with dropout applied.
+    """
+    if dropout_prob is None or dropout_prob == 0.0:
+        return input_tensor
+
+    output = tf.nn.dropout(input_tensor, 1.0 - dropout_prob)
+    return output
