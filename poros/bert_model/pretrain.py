8f7a5e1e2a0140d7ae8aecd884fb6681f7896ce5
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2018-12-18 10:39:54 +0800

    delete tf.flags because it is a global variable

--- poros/bert_model/pretrain.py
+++ poros/bert_model/pretrain.py
@@ -23,88 +23,130 @@ from poros.bert_model import modeling
 from poros.bert_model import optimization
 import tensorflow as tf
 
-flags = tf.flags
-
-FLAGS = flags.FLAGS
+_flag = dict()
 
 ## Required parameters
+
+_flag["bert_config"] = None
+_flag["input_file"] = None
+_flag["output_dir"] = None
+
+"""
 flags.DEFINE_string(
     "bert_config_file", None,
     "The config json file corresponding to the pre-trained BERT model. "
     "This specifies the model architecture.")
+"""
 
+"""
 flags.DEFINE_string(
     "input_file", None,
     "Input TF example files (can be a glob or comma separated).")
+"""
 
+"""
 flags.DEFINE_string(
     "output_dir", None,
     "The output directory where the model checkpoints will be written.")
+"""
+_flag["output_dir"] = None
 
 ## Other parameters
+"""
 flags.DEFINE_string(
     "init_checkpoint", None,
     "Initial checkpoint (usually from a pre-trained BERT model).")
+"""
+_flag["init_checkpoint"] = None
 
+"""
 flags.DEFINE_integer(
     "max_seq_length", 128,
     "The maximum total input sequence length after WordPiece tokenization. "
     "Sequences longer than this will be truncated, and sequences shorter "
     "than this will be padded. Must match data generation.")
+"""
+_flag["max_seq_length"] = 128
 
+"""
 flags.DEFINE_integer(
     "max_predictions_per_seq", 20,
     "Maximum number of masked LM predictions per sequence. "
     "Must match data generation.")
+"""
+_flag["max_predictions_per_seq"] = 20
 
-flags.DEFINE_bool("do_train", False, "Whether to run training.")
+#flags.DEFINE_bool("do_train", False, "Whether to run training.")
+_flag["do_train"] = False
 
-flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
+#flags.DEFINE_bool("do_eval", False, "Whether to run eval on the dev set.")
+_flag["do_eval"] = False
 
-flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
+#flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
+_flag["train_batch_size"] = 32
 
-flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
+#flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
+_flag["eval_batch_size"] = 8
 
-flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
+#flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
+_flag["learning_rate"] = 5e-5
 
-flags.DEFINE_integer("num_train_steps", 100000, "Number of training steps.")
+#flags.DEFINE_integer("num_train_steps", 100000, "Number of training steps.")
+_flag["num_train_steps"] = 100000
 
-flags.DEFINE_integer("num_warmup_steps", 10000, "Number of warmup steps.")
+#flags.DEFINE_integer("num_warmup_steps", 10000, "Number of warmup steps.")
+_flag["num_warmup_steps"] = 10000
 
+"""
 flags.DEFINE_integer("save_checkpoints_steps", 1000,
                      "How often to save the model checkpoint.")
+"""
+_flag["save_checkpoints_steps"] = 1000
 
+"""
 flags.DEFINE_integer("iterations_per_loop", 1000,
                      "How many steps to make in each estimator call.")
+"""
+_flag["iterations_per_loop"] = 1000
 
-flags.DEFINE_integer("max_eval_steps", 100, "Maximum number of eval steps.")
+#flags.DEFINE_integer("max_eval_steps", 100, "Maximum number of eval steps.")
+_flag["max_eval_steps"] = 100
 
-flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
+#flags.DEFINE_bool("use_tpu", False, "Whether to use TPU or GPU/CPU.")
+_flag["use_tpu"] = False
 
-tf.flags.DEFINE_string(
+"""tf.flags.DEFINE_string(
     "tpu_name", None,
     "The Cloud TPU to use for training. This should be either the name "
     "used when creating the Cloud TPU, or a grpc://ip.address.of.tpu:8470 "
     "url.")
+"""
+_flag["tpu_name"] = None
 
+"""
 tf.flags.DEFINE_string(
     "tpu_zone", None,
     "[Optional] GCE zone where the Cloud TPU is located in. If not "
     "specified, we will attempt to automatically detect the GCE project from "
     "metadata.")
+"""
+_flag["tpu_zone"] = None
 
-tf.flags.DEFINE_string(
-    "gcp_project", None,
-    "[Optional] Project name for the Cloud TPU-enabled project. If not "
-    "specified, we will attempt to automatically detect the GCE project from "
-    "metadata.")
+#tf.flags.DEFINE_string(
+#    "gcp_project", None,
+#    "[Optional] Project name for the Cloud TPU-enabled project. If not "
+#    "specified, we will attempt to automatically detect the GCE project from "
+#    "metadata.")
+_flag["gcp_project"] = None
 
-tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
+#tf.flags.DEFINE_string("master", None, "[Optional] TensorFlow master URL.")
+_flag["master"] = None
 
-flags.DEFINE_integer(
-    "num_tpu_cores", 8,
-    "Only used if `use_tpu` is True. Total number of TPU cores to use.")
+#flags.DEFINE_integer(
+#    "num_tpu_cores", 8,
+#    "Only used if `use_tpu` is True. Total number of TPU cores to use.")
 
+_flag["num_tpu_cores"] = 8
 
 def model_fn_builder(bert_config, init_checkpoint, learning_rate,
                      num_train_steps, num_warmup_steps, use_tpu,
@@ -406,15 +448,15 @@ def _decode_record(record, name_to_features):
 def main(_):
     tf.logging.set_verbosity(tf.logging.INFO)
 
-    if not FLAGS.do_train and not FLAGS.do_eval:
+    if not _flag["do_train"] and not _flag["do_eval"]:
         raise ValueError("At least one of `do_train` or `do_eval` must be True.")
 
-    bert_config = modeling.BertConfig.from_json_file(FLAGS.bert_config_file)
+    bert_config = modeling.BertConfig.from_json_file(_flag["bert_config_file"])
 
-    tf.gfile.MakeDirs(FLAGS.output_dir)
+    tf.gfile.MakeDirs(_flag["output_dir"])
 
     input_files = []
-    for input_pattern in FLAGS.input_file.split(","):
+    for input_pattern in _flag["input_file"].split(","):
         input_files.extend(tf.gfile.Glob(input_pattern))
 
     tf.logging.info("*** Input Files ***")
@@ -422,63 +464,63 @@ def main(_):
         tf.logging.info("  %s" % input_file)
 
     tpu_cluster_resolver = None
-    if FLAGS.use_tpu and FLAGS.tpu_name:
+    if _flag["use_tpu"] and _flag["tpu_name"]:
         tpu_cluster_resolver = tf.contrib.cluster_resolver.TPUClusterResolver(
-            FLAGS.tpu_name, zone=FLAGS.tpu_zone, project=FLAGS.gcp_project)
+            _flag["tpu_name"], zone=_flag["tpu_zone"], project=_flag["gcp_project"])
 
     is_per_host = tf.contrib.tpu.InputPipelineConfig.PER_HOST_V2
     run_config = tf.contrib.tpu.RunConfig(
         cluster=tpu_cluster_resolver,
-        master=FLAGS.master,
-        model_dir=FLAGS.output_dir,
-        save_checkpoints_steps=FLAGS.save_checkpoints_steps,
+        master=_flag["master"],
+        model_dir=_flag["output_dir"],
+        save_checkpoints_steps=_flag["save_checkpoints_steps"],
         tpu_config=tf.contrib.tpu.TPUConfig(
-            iterations_per_loop=FLAGS.iterations_per_loop,
-            num_shards=FLAGS.num_tpu_cores,
+            iterations_per_loop=_flag["iterations_per_loop"],
+            num_shards=_flag["num_tpu_cores"],
             per_host_input_for_training=is_per_host))
 
     model_fn = model_fn_builder(
         bert_config=bert_config,
-        init_checkpoint=FLAGS.init_checkpoint,
-        learning_rate=FLAGS.learning_rate,
-        num_train_steps=FLAGS.num_train_steps,
-        num_warmup_steps=FLAGS.num_warmup_steps,
-        use_tpu=FLAGS.use_tpu,
-        use_one_hot_embeddings=FLAGS.use_tpu)
+        init_checkpoint=_flag["init_checkpoint"],
+        learning_rate=_flag["learning_rate"],
+        num_train_steps=_flag["num_train_steps"],
+        num_warmup_steps=_flag["num_warmup_steps"],
+        use_tpu=_flag["use_tpu"],
+        use_one_hot_embeddings=_flag["use_tpu"])
 
     # If TPU is not available, this will fall back to normal Estimator on CPU
     # or GPU.
     estimator = tf.contrib.tpu.TPUEstimator(
-        use_tpu=FLAGS.use_tpu,
+        use_tpu=_flag["use_tpu"],
         model_fn=model_fn,
         config=run_config,
-        train_batch_size=FLAGS.train_batch_size,
-        eval_batch_size=FLAGS.eval_batch_size)
+        train_batch_size=_flag["train_batch_size"],
+        eval_batch_size=_flag["eval_batch_size"])
 
-    if FLAGS.do_train:
+    if _flag["do_train"]:
         tf.logging.info("***** Running training *****")
-        tf.logging.info("  Batch size = %d", FLAGS.train_batch_size)
+        tf.logging.info("  Batch size = %d", _flag["train_batch_size"])
         train_input_fn = input_fn_builder(
             input_files=input_files,
-            max_seq_length=FLAGS.max_seq_length,
-            max_predictions_per_seq=FLAGS.max_predictions_per_seq,
+            max_seq_length=_flag["max_seq_length"],
+            max_predictions_per_seq=_flag["max_predictions_per_seq"],
             is_training=True)
-        estimator.train(input_fn=train_input_fn, max_steps=FLAGS.num_train_steps)
+        estimator.train(input_fn=train_input_fn, max_steps=_flag["num_train_steps"])
 
-    if FLAGS.do_eval:
+    if _flag["do_eval"]:
         tf.logging.info("***** Running evaluation *****")
-        tf.logging.info("  Batch size = %d", FLAGS.eval_batch_size)
+        tf.logging.info("  Batch size = %d", _flag["eval_batch_size"])
 
         eval_input_fn = input_fn_builder(
             input_files=input_files,
-            max_seq_length=FLAGS.max_seq_length,
-            max_predictions_per_seq=FLAGS.max_predictions_per_seq,
+            max_seq_length=_flag["max_seq_length"],
+            max_predictions_per_seq=_flag["max_predictions_per_seq"],
             is_training=False)
 
         result = estimator.evaluate(
-            input_fn=eval_input_fn, steps=FLAGS.max_eval_steps)
+            input_fn=eval_input_fn, steps=_flag["max_eval_steps"])
 
-        output_eval_file = os.path.join(FLAGS.output_dir, "eval_results.txt")
+        output_eval_file = os.path.join(_flag["output_dir"], "eval_results.txt")
         with tf.gfile.GFile(output_eval_file, "w") as writer:
             tf.logging.info("***** Eval results *****")
             for key in sorted(result.keys()):
@@ -489,21 +531,21 @@ def main(_):
 def run(input_file, bert_config_file, output_dir, max_seq_length=128, max_predictions_per_seq=20, do_train=False, do_eval=True,
         train_batch_size=32, eval_batch_size=8, learning_rate=5e-5, num_train_steps=100000, num_warmup_steps=1000,
         save_checkpoints_steps=1000, iterations_per_loop=1000, max_eval_steps=100):
-    FLAGS.input_file = input_file
-    FLAGS.bert_config_file = bert_config_file
-    FLAGS.output_dir = output_dir
-    FLAGS.max_seq_length = max_seq_length
-    FLAGS.max_predictions_per_seq = max_predictions_per_seq
-    FLAGS.do_train = do_train
-    FLAGS.do_eval = do_eval
-    FLAGS.train_batch_size = train_batch_size
-    FLAGS.eval_batch_size = eval_batch_size
-    FLAGS.learning_rate = learning_rate
-    FLAGS.num_train_steps = num_train_steps
-    FLAGS.num_warmup_steps = num_warmup_steps
-    FLAGS.save_checkpoints_steps = save_checkpoints_steps
-    FLAGS.iterations_per_loop = iterations_per_loop
-    FLAGS.max_eval_steps = max_eval_steps
+    _flag["input_file"] = input_file
+    _flag["bert_config_file"] = bert_config_file
+    _flag["output_dir"] = output_dir
+    _flag["max_seq_length"] = max_seq_length
+    _flag["max_predictions_per_seq"] = max_predictions_per_seq
+    _flag["do_train"] = do_train
+    _flag["do_eval"] = do_eval
+    _flag["train_batch_size"] = train_batch_size
+    _flag["eval_batch_size"] = eval_batch_size
+    _flag["learning_rate"] = learning_rate
+    _flag["num_train_steps"] = num_train_steps
+    _flag["num_warmup_steps"] = num_warmup_steps
+    _flag["save_checkpoints_steps"] = save_checkpoints_steps
+    _flag["iterations_per_loop"] = iterations_per_loop
+    _flag["max_eval_steps"] = max_eval_steps
     tf.app.run()
 
 
