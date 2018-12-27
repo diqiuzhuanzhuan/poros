3bfd0419155e560b01c2510a158a7d12dc887501
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2018-12-27 14:00:24 +0800

    add alpha and beta parameter

--- poros/bert_model/pretrain.py
+++ poros/bert_model/pretrain.py
@@ -189,7 +189,7 @@ def model_fn_builder(bert_config, init_checkpoint, learning_rate,
          next_sentence_log_probs) = get_next_sentence_output(
             bert_config, model.get_pooled_output(), next_sentence_labels)
 
-        total_loss = masked_lm_loss + next_sentence_loss
+        total_loss = _flag["alpha"] * masked_lm_loss + _flag["beta"] * next_sentence_loss
 
         tvars = tf.trainable_variables()
 
@@ -535,7 +535,7 @@ def main(_):
 
 def run(input_file, bert_config_file, output_dir, max_seq_length=128, max_predictions_per_seq=20, do_train=False, do_eval=True,
         train_batch_size=32, eval_batch_size=8, learning_rate=5e-5, num_train_steps=100000, num_warmup_steps=1000,
-        save_checkpoints_steps=1000, iterations_per_loop=1000, max_eval_steps=100, log_step_count_steps=100):
+        save_checkpoints_steps=1000, iterations_per_loop=1000, max_eval_steps=100, log_step_count_steps=100, alpha=1.0, beta=1.0):
     _flag["input_file"] = input_file
     _flag["bert_config_file"] = bert_config_file
     _flag["output_dir"] = output_dir
@@ -552,6 +552,8 @@ def run(input_file, bert_config_file, output_dir, max_seq_length=128, max_predic
     _flag["iterations_per_loop"] = iterations_per_loop
     _flag["max_eval_steps"] = max_eval_steps
     _flag["log_step_count_steps"] = log_step_count_steps
+    _flag["alpha"] = alpha
+    _flag["beta"] = beta
     main(None)
 
 
