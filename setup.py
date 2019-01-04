6cbc4d9150497d928b2e17fd458b8b2f2de17dab
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2019-01-04 18:59:52 +0800

    update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="poros",
-    version="0.0.18",
+    version="0.0.19",
     author="Feynman",
     author_email="diqiuzhuanzhuan@gmail.com",
     description="some useful code",
