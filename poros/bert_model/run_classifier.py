3a080a25ce7cec9a00f26da28c475572fd684455
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2019-01-22 11:39:51 +0800

    change loss

--- poros/bert_model/run_classifier.py
+++ poros/bert_model/run_classifier.py
@@ -363,7 +363,7 @@ def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
 
         #per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
         loss = about_loss.focal_loss(log_probs, one_hot_labels)
-        per_example_loss = loss
+        loss, per_example_loss = tf.metrics.mean(loss)
         #loss = tf.reduce_mean(per_example_loss)
 
         return (loss, per_example_loss, logits, probabilities, arg_max)
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="poros",
-    version="0.0.26",
+    version="0.0.28",
     author="Feynman",
     author_email="diqiuzhuanzhuan@gmail.com",
     description="some useful code",
