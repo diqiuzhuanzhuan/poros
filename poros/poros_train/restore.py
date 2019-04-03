69981fbf3fc73f37d77036d679920c070779e21a
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2019-04-03 16:07:42 +0800

    print all variables when restore a model

--- poros/poros_train/restore.py
+++ poros/poros_train/restore.py
@@ -41,3 +41,10 @@ def restore(init_checkpoint):
     tvars = tf.trainable_variables()
     assignment_map, initialized_variable_names = get_assignment_map_from_checkpoint(tvars, init_checkpoint)
     tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+    tf.logging.info("**** Trainable Variables ****")
+    for var in tvars:
+        init_string = ""
+        if var.name in initialized_variable_names:
+            init_string = ", *INIT_FROM_CKPT*"
+        tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape,
+                        init_string)
