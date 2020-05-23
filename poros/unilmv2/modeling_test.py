09bd7a9fc77d94960f44fd501b37b18eea7cc381
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2020-05-23 17:51:04 +0800

    add position embedding layer test case

--- poros/unilmv2/modeling_test.py
+++ poros/unilmv2/modeling_test.py
@@ -6,6 +6,9 @@ email: diqiuzhuanzhuan@gmail.com
 
 """
 import unittest
+from unilmv2.modeling import PositionEmbeddingLayer
+import tensorflow as tf
+from poros_dataset.about_tensor import get_shape
 
 
 class ModelingTest(unittest.TestCase):
@@ -14,4 +17,9 @@ class ModelingTest(unittest.TestCase):
       pass
 
    def test_position_embedding_layer(self):
-      pass
+      position_embedding_layer = PositionEmbeddingLayer(position_size=128, embedding_size=128)
+      input_id = tf.constant([[1, 2, 3, 4]])
+      look_up, embedding_table = position_embedding_layer(input_id)
+      self.assertEqual(get_shape(look_up), [1, 4, 128])
+      self.assertEqual(get_shape(embedding_table), [128, 128])
+
