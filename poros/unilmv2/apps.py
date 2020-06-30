fix bug in attention mask

poros/unilmv2/apps.py
@@ -15,6 +15,7 @@ from poros.unilmv2 import (
     MaskLmLayer,
     PseudoMaskLmLayer
 )
+from tensorflow.keras.callbacks import ReduceLROnPlateau
 
 
 def pretrain():
@@ -27,8 +28,11 @@ def pretrain():
     json_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_data", "bert_config.json")
     unilmv2_config = Unilmv2Config.from_json_file(json_file)
     unilmv2_model = Unilmv2Model(config=unilmv2_config, is_training=True)
-    unilmv2_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001))
-    unilmv2_model.fit(dataset, epochs=2000, steps_per_epoch=15)
+    reduce_lr = ReduceLROnPlateau(monitor='masked_lm_loss', factor=0.8,
+                                  patience=10, min_lr=0)
+
+    unilmv2_model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.1))
+    unilmv2_model.fit(dataset, epochs=2000, steps_per_epoch=15, callbacks=[reduce_lr])
 
 
 class Unilmv2Model(tf.keras.Model):

poros/unilmv2/dataman.py
@@ -157,6 +157,7 @@ def create_attention_mask(input_ids, input_mask, pseudo_masked_index, pseudo_mas
     non_zero = np.count_nonzero(input_mask)
     mask_matrix[non_zero:, :] = 0
     normal_text_can_be_seen = []
+    pseudo_masked_sub_list_len = pseudo_masked_sub_list_len[0:np.count_nonzero(pseudo_masked_sub_list_len)]
 
     for block_index in pseudo_masked_sub_list_len[::-1]:
         sub_pseudo_index = pseudo_masked_index[-block_index:]
@@ -176,7 +177,10 @@ def create_attention_mask(input_ids, input_mask, pseudo_masked_index, pseudo_mas
     return mask_matrix
 
 
-def add_attention_mask(features):
+def add_attention_mask(features, is_training=False):
+    if not is_training:
+        features["attention_mask"] = None
+        return features
     input_ids = features["input_ids"]
     input_mask = features["input_mask"]
     pseudo_masked_index = features["pseudo_masked_index"]
@@ -555,13 +559,12 @@ class PreTrainingDataMan(object):
                                                                  cycle_length=cycle_length,
                                                                  sloppy=is_training
                                                                  ))
-            d = d.shuffle(buffer_size=100)
+            #d = d.shuffle(buffer_size=100)
         else:
             d = tf.data.TFRecordDataset(input_files)
 
         d = d.map(lambda record: about_tfrecord.parse_example(record, name_to_features))
-        if is_training:
-            d = d.map(lambda x: add_attention_mask(x)).repeat()
+        d = d.map(lambda x: add_attention_mask(x, is_training)).repeat()
 
         d = d.batch(batch_size=batch_size, drop_remainder=True)
 