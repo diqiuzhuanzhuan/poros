b45822951d57498c96a54abf86c1b309f2002028
MALONG633 <>
2020-12-01 09:00:22 +0800

    更新poros版本

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with codecs.open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="poros",
-    version="0.0.53",
+    version="0.0.54",
     author="Feynman",
     author_email="diqiuzhuanzhuan@gmail.com",
     description="some useful code",
