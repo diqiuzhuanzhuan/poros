fd47494941f1cb559e7a96453fee6df471657b0e
diqiuzhuanzhuan <diqiuzhuanzhuan@gmail.com>
2022-07-26 14:20:11 +0800

    add import in top __init__.py

--- poros/__init__.py
+++ poros/__init__.py
@@ -12,5 +12,6 @@ from poros import poros_dataset
 from poros import poros_loss
 from poros import poros_train
 from poros import poros_metrics
+from poros import poros_layers
 from poros import ner
 from poros import cf
